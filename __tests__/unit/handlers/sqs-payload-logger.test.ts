264e5762c5331217e05483807a701650df41b7a7
Lewis Dale <lewis.dale@madetech.com>
2020-12-07 16:17:49 +0000

    Test and implementation for multiple messages

--- __tests__/unit/handlers/sqs-payload-logger.test.ts
+++ __tests__/unit/handlers/sqs-payload-logger.test.ts
@@ -55,4 +55,63 @@ describe('Test for sqs-payload-logger', function () {
         .toEqual(`https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`);
     expect(JSON.parse(request.config.data)).toEqual(event.Records[0]);
   });
+  it('should call the MQ endpoint for each record supplied by the event', async () => {
+      process.env.MQ_HOST = 'a-host';
+      process.env.MQ_PORT = '1999';
+      process.env.MQ_QUEUE = 'my-fake-queue';
+      process.env.MQ_USER = 'a-user';
+      process.env.MQ_PASSWORD = 'a-password';
+
+      const event : SQSEvent = {
+          Records: [
+              {
+                  "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
+                  "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
+                  "body": "Test message.",
+                  "attributes": {
+                      "ApproximateReceiveCount": "1",
+                      "SentTimestamp": "1545082649183",
+                      "SenderId": "AIDAIENQZJOLO23YVJ4VO",
+                      "ApproximateFirstReceiveTimestamp": "1545082649185"
+                  },
+                  "messageAttributes": {},
+                  "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
+                  "eventSource": "aws:sqs",
+                  "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
+                  "awsRegion": "us-east-2"
+              },
+              {
+                  "messageId": "2e1424d4-f796-459a-8184-9c92662be6da",
+                  "receiptHandle": "AQEBzWwaftRI0KuVm4tP+/7q1rGgNqicHq...",
+                  "body": "Test message.",
+                  "attributes": {
+                      "ApproximateReceiveCount": "1",
+                      "SentTimestamp": "1545082650636",
+                      "SenderId": "AIDAIENQZJOLO23YVJ4VO",
+                      "ApproximateFirstReceiveTimestamp": "1545082650649"
+                  },
+                  "messageAttributes": {},
+                  "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
+                  "eventSource": "aws:sqs",
+                  "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
+                  "awsRegion": "us-east-2"
+              }
+          ]
+      };
+      moxios.stubRequest(
+          `https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`,
+          {
+              status: 200,
+          }
+      );
+      await pushMessageToMq(event);
+
+      event.Records.forEach((record, index) => {
+          const request = moxios.requests.at(index);
+          expect(request).toBeDefined();
+          expect(request.config.method).toEqual("post");
+          expect(request.url)
+              .toEqual(`https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`);
+      });
+  });
 });
--- src/handlers/sqs-payload-logger.ts
+++ src/handlers/sqs-payload-logger.ts
@@ -5,16 +5,17 @@ import axios from 'axios';
  * A Lambda function that logs the payload received from SQS.
  */
 export const pushMessageToMq = async (event: SQSEvent): Promise<void> => {
-    const record = event.Records[0];
-    await axios.post(
-        `https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`,
-        record,
-        {
-            headers: {
-                "ibm-mq-rest-csrf-token": "blank",
-                "Content-Type": "text/plain;charset=utf-8",
-                "Authorization": `Basic ${process.env.MQ_USER}:${process.env.MQ_PASSWORD}`
-            },
-        }
-    )
+    event.Records.forEach((record) => {
+        axios.post(
+            `https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`,
+            record,
+            {
+                headers: {
+                    "ibm-mq-rest-csrf-token": "blank",
+                    "Content-Type": "text/plain;charset=utf-8",
+                    "Authorization": `Basic ${process.env.MQ_USER}:${process.env.MQ_PASSWORD}`
+                },
+            }
+        );
+    });
 }
