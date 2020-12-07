c7d891bb94720a06e656fd9792ed5d90077e6a02
Lewis Dale <lewis.dale@madetech.com>
2020-12-07 15:48:54 +0000

    Finished test for first pass at lambda

--- __tests__/unit/handlers/sqs-payload-logger.test.ts
+++ __tests__/unit/handlers/sqs-payload-logger.test.ts
@@ -1,14 +1,53 @@
+import {SQSEvent} from "aws-lambda";
+import * as moxios from 'moxios';
+
 import { pushMessageToMq } from "../../../src/handlers/sqs-payload-logger";
+
 describe('Test for sqs-payload-logger', function () {
+    beforeEach(() => {
+        moxios.install();
+    });
+
+    afterEach(() => {
+        moxios.uninstall();
+    });
+
   // Example curl post message
   // curl -i -k https://localhost:9443/ibmmq/rest/v1/messaging/qmgr/QM1/queue/DEV.QUEUE.1/message -X POST -u user:password -H "ibm-mq-rest-csrf-token: blank" -H "Content-Type: text/plain;charset=utf-8" -d "Hello World!"
-  it('a test', () => {
+  it('should call the MQ API when a message event is triggered', () => {
     process.env.MQ_HOST = 'a-host';
     process.env.MQ_PORT = '1999';
     process.env.MQ_QUEUE = 'my-fake-queue';
-    pushMessageToMq(null);
-    expect(axios.post).toBeCalledWith({
-      url: `http://${env.MQ_HOST}:${env.MQ_PORT}....`
-    });
+    process.env.MQ_USER = 'a-user';
+    process.env.MQ_PASSWORD = 'a-password';
+
+     const event : SQSEvent = {
+          Records: [
+              {
+                  "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
+                  "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
+                  "body": "Test message.",
+                  "attributes": {
+                      "ApproximateReceiveCount": "1",
+                      "SentTimestamp": "1545082649183",
+                      "SenderId": "AIDAIENQZJOLO23YVJ4VO",
+                      "ApproximateFirstReceiveTimestamp": "1545082649185"
+                  },
+                  "messageAttributes": {},
+                  "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
+                  "eventSource": "aws:sqs",
+                  "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
+                  "awsRegion": "us-east-2"
+              },
+        ]
+     };
+
+    pushMessageToMq(event);
+
+    const request = moxios.requests.mostRecent();
+    expect(request.config.method).toEqual("post");
+    expect(request.url)
+        .toEqual(`https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`);
+    expect(request.config.data).toEqual(event.Records[0]);
   });
 });
