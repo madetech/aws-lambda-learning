32e30a6d87829a8093451ba48ae91b00ef3b6552
Christopher Parsons <chris.p@madetech.com>
2020-12-07 15:11:04 +0000

    Setup jest for typescript and remove weird unhelpful default test

---
+++ __tests__/unit/handlers/sqs-payload-logger.test.ts
@@ -0,0 +1,5 @@
+describe('Test for sqs-payload-logger', function () {
+  // Example curl post message
+  // curl -i -k https://localhost:9443/ibmmq/rest/v1/messaging/qmgr/QM1/queue/DEV.QUEUE.1/message -X POST -u user:password -H "ibm-mq-rest-csrf-token: blank" -H "Content-Type: text/plain;charset=utf-8" -d "Hello World!"
+
+});
