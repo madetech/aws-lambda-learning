eff494d4f4696fa0845d22ce7925804f62e382cf
Lewis Dale <lewis.dale@madetech.com>
2020-12-07 16:07:53 +0000

    Passing first test for SQS to MQ event

--- src/handlers/sqs-payload-logger.ts
+++ src/handlers/sqs-payload-logger.ts
@@ -6,4 +6,15 @@ import axios from 'axios';
  */
 export const pushMessageToMq = async (event: SQSEvent): Promise<void> => {
     const record = event.Records[0];
+    await axios.post(
+        `https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`,
+        record,
+        {
+            headers: {
+                "ibm-mq-rest-csrf-token": "blank",
+                "Content-Type": "text/plain;charset=utf-8",
+                "Authorization": `Basic ${process.env.MQ_USER}:${process.env.MQ_PASSWORD}`
+            },
+        }
+    )
 }
