792a9c0185c86892d6cc142693c46067eb2f1113
Ninamma Rai <ninamma.rai@outlook.com>
2020-12-16 14:39:05 +0000

    Using newer IBM MQ REST API, axiosInstance so SSL errors can be ignored
    Removing the catch to push to dead queue as lambda does that itself
    Bumping down axios version as we need to use the same version as
    moxios to avoid type error with AxiosInstancs

--- __tests__/unit/handlers/sqs-payload-logger.test.ts
+++ __tests__/unit/handlers/sqs-payload-logger.test.ts
@@ -1,39 +1,31 @@
 import {SQSEvent} from "aws-lambda";
+import axios, { AxiosInstance } from 'axios';
 import * as moxios from 'moxios';
 import { pushMessageToMq } from "../../../src/handlers/sqs-payload-logger";
-import * as aws from 'aws-sdk';
-
-const SQSMocked = {
-  sendMessage: jest.fn((params: aws.SQS.Types.SendMessageRequest) => ({ promise: jest.fn() })),
-  promise: jest.fn(),
-};
-
-jest.mock('aws-sdk', () => {
-  return {
-    SQS: jest.fn(() => SQSMocked)
-  };
-});
-
-
 
 describe('Test for sqs-payload-logger', function () {
+  let axiosInstance: AxiosInstance;
+
     beforeEach(() => {
-        moxios.install();
+        axiosInstance = axios.create();
+        moxios.install(axiosInstance);
     });
 
     afterEach(() => {
-        moxios.uninstall();
+        moxios.uninstall(axiosInstance);
     });
 
-  // Example curl post message
-  // curl -i -k https://localhost:9443/ibmmq/rest/v1/messaging/qmgr/QM1/queue/DEV.QUEUE.1/message -X POST -u user:password -H "ibm-mq-rest-csrf-token: blank" -H "Content-Type: text/plain;charset=utf-8" -d "Hello World!"
-  it('should call the MQ API when a message event is triggered', async () => {
     process.env.MQ_HOST = 'a-host';
     process.env.MQ_PORT = '1999';
+    process.env.MQ_QUEUE_MANAGER = 'QMGR';
     process.env.MQ_QUEUE = 'my-fake-queue';
     process.env.MQ_USER = 'a-user';
     process.env.MQ_PASSWORD = 'a-password';
 
+    const URL = `https://${process.env.MQ_USER}:${process.env.MQ_PASSWORD}@${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v2/messaging/qmgr/${process.env.MQ_QUEUE_MANAGER}/queue/${process.env.MQ_QUEUE}/message`;
+  
+    it('should call the MQ API when a message event is triggered', async () => {
+    
     const event : SQSEvent = {
       Records: [
           {
@@ -54,20 +46,22 @@ describe('Test for sqs-payload-logger', function () {
           },
       ]
     };
+
     moxios.stubRequest(
-     `https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`,
-     {
+      URL,
+      {
         status: 200,
-     }
+      }
     );
-    await pushMessageToMq(event);
+    await pushMessageToMq(event, axiosInstance);
 
     const request = moxios.requests.mostRecent();
     expect(request.config.method).toEqual("post");
     expect(request.url)
-        .toEqual(`https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`);
+        .toEqual(URL);
     expect(JSON.parse(request.config.data)).toEqual(event.Records[0]);
   });
+
   it('should call the MQ endpoint for each record supplied by the event', async () => {
       process.env.MQ_HOST = 'a-host';
       process.env.MQ_PORT = '1999';
@@ -112,61 +106,19 @@ describe('Test for sqs-payload-logger', function () {
           ]
       };
       moxios.stubRequest(
-          `https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`,
+          URL,
           {
               status: 200,
           }
       );
-      await pushMessageToMq(event);
+      await pushMessageToMq(event, axiosInstance);
 
       event.Records.forEach((record, index) => {
           const request = moxios.requests.at(index);
           expect(request).toBeDefined();
           expect(request.config.method).toEqual("post");
           expect(request.url)
-              .toEqual(`https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`);
+              .toEqual(URL);
       });
-  });
-  it('should push message to SQS dead letter queue when pushing to MQ errors', async () => {
-    process.env.MQ_HOST = 'a-host';
-    process.env.MQ_PORT = '1999';
-    process.env.MQ_QUEUE = 'my-fake-queue';
-    process.env.MQ_USER = 'a-user';
-    process.env.MQ_PASSWORD = 'a-password';
-    process.env.DEAD_LETTER_QUEUE = 'dead-letter-queue';
-    const event : SQSEvent = {
-        Records: [
-            {
-                "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
-                "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
-                "body": "Test message.",
-                "attributes": {
-                    "ApproximateReceiveCount": "1",
-                    "SentTimestamp": "1545082649183",
-                    "SenderId": "AIDAIENQZJOLO23YVJ4VO",
-                    "ApproximateFirstReceiveTimestamp": "1545082649185"
-                },
-                "messageAttributes": {},
-                "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
-                "eventSource": "aws:sqs",
-                "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
-                "awsRegion": "us-east-2"
-            }
-        ]
-    };
-    moxios.stubRequest(
-        `https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`,
-        {
-          status: 500
-        }
-    );
-    
-    await pushMessageToMq(event);
-    
-    expect(SQSMocked.sendMessage).toBeCalledWith({
-        DelaySeconds: 10,
-        MessageBody: JSON.stringify(event.Records[0]),
-        QueueUrl: 'dead-letter-queue'
-    });
   });
 });
--- src/handlers/sqs-payload-logger.ts
+++ src/handlers/sqs-payload-logger.ts
@@ -1,36 +1,28 @@
-import {SQSEvent} from "aws-lambda";
-import axios from 'axios';
-import * as aws from 'aws-sdk';
+import { SQSEvent } from "aws-lambda";
+import * as https from 'https';
+import axios, { AxiosInstance } from 'axios';
 
-/**
- * A Lambda function that logs the payload received from SQS.
- */
-export const pushMessageToMq = async (event: SQSEvent): Promise<void> => {
-    console.info(`Received ${event.Records.length} records`);
-    await Promise.all(event.Records.map(async (record) => {
-        console.info("Posting record to MQ");
-        await axios.post(
-            `https://${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/QM1/queue/${process.env.MQ_QUEUE}/message`,
-            record,
-            {
-                headers: {
-                    "ibm-mq-rest-csrf-token": "blank",
-                    "Content-Type": "text/plain;charset=utf-8",
-                    "Authorization": `Basic ${process.env.MQ_USER}:${process.env.MQ_PASSWORD}`
-                },
-            }
-        ).catch(async () => {
-            console.info("Failed to post to MQ");
-            console.info(`Pushing to ${process.env.DEAD_LETTER_QUEUE}`)
-            const SQS = new aws.SQS();
-            const params = {
-                // Remove DelaySeconds parameter and value for FIFO queues
-               DelaySeconds: 10,
-               MessageBody: JSON.stringify(record),
-               QueueUrl: process.env.DEAD_LETTER_QUEUE
-             };
+const axiosInstance = axios.create({
+    httpsAgent: new https.Agent({  
+      rejectUnauthorized: false
+  })
+});
 
-            await SQS.sendMessage(params).promise()
-        });
-    }));
+export const pushMessageToMq = async (event: SQSEvent, instance: AxiosInstance = axiosInstance ): Promise<void> => {
+  console.info(`Received ${event.Records.length} records`);
+  await Promise.all(event.Records.map(async (record) => {
+    console.info("Posting record to MQ");
+    const URL = `https://${process.env.MQ_USER}:${process.env.MQ_PASSWORD}@${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v2/messaging/qmgr/${process.env.MQ_QUEUE_MANAGER}/queue/${process.env.MQ_QUEUE}/message`;
+    console.log(`Posting to: ${URL}`);
+    await instance.post(
+      URL,
+      record,
+      {
+        headers: {
+          "ibm-mq-rest-csrf-token": "blank",
+          "Content-Type": "text/plain;charset=utf-8"
+        },
+      }
+    )
+  }));
 }
