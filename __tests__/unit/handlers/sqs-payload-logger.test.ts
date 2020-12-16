6a4806bc51363c0c1b3c95638b2691e6ace63224
Ninamma Rai <ninamma.rai@outlook.com>
2020-12-16 14:48:25 +0000

    Fixing indentation

--- __tests__/unit/handlers/sqs-payload-logger.test.ts
+++ __tests__/unit/handlers/sqs-payload-logger.test.ts
@@ -6,47 +6,105 @@ import { pushMessageToMq } from "../../../src/handlers/sqs-payload-logger";
 describe('Test for sqs-payload-logger', function () {
   let axiosInstance: AxiosInstance;
 
-    beforeEach(() => {
-        axiosInstance = axios.create();
-        moxios.install(axiosInstance);
-    });
+  beforeEach(() => {
+    axiosInstance = axios.create();
+    moxios.install(axiosInstance);
+  });
 
-    afterEach(() => {
-        moxios.uninstall(axiosInstance);
-    });
+  afterEach(() => {
+    moxios.uninstall(axiosInstance);
+  });
 
+  process.env.MQ_HOST = 'a-host';
+  process.env.MQ_PORT = '1999';
+  process.env.MQ_QUEUE_MANAGER = 'QMGR';
+  process.env.MQ_QUEUE = 'my-fake-queue';
+  process.env.MQ_USER = 'a-user';
+  process.env.MQ_PASSWORD = 'a-password';
+
+  const URL = `https://${process.env.MQ_USER}:${process.env.MQ_PASSWORD}@${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v2/messaging/qmgr/${process.env.MQ_QUEUE_MANAGER}/queue/${process.env.MQ_QUEUE}/message`;
+  
+  it('should call the MQ API when a message event is triggered', async () => {
+  
+  const event : SQSEvent = {
+    Records: [
+      {
+        "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
+        "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
+        "body": "Test message.",
+        "attributes": {
+          "ApproximateReceiveCount": "1",
+          "SentTimestamp": "1545082649183",
+          "SenderId": "AIDAIENQZJOLO23YVJ4VO",
+          "ApproximateFirstReceiveTimestamp": "1545082649185"
+        },
+        "messageAttributes": {},
+        "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
+        "eventSource": "aws:sqs",
+        "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
+        "awsRegion": "us-east-2"
+      },
+    ]
+  };
+
+  moxios.stubRequest(
+    URL,
+    {
+    status: 200,
+    }
+  );
+  await pushMessageToMq(event, axiosInstance);
+
+  const request = moxios.requests.mostRecent();
+  expect(request.config.method).toEqual("post");
+  expect(request.url)
+    .toEqual(URL);
+  expect(JSON.parse(request.config.data)).toEqual(event.Records[0]);
+  });
+
+  it('should call the MQ endpoint for each record supplied by the event', async () => {
     process.env.MQ_HOST = 'a-host';
     process.env.MQ_PORT = '1999';
-    process.env.MQ_QUEUE_MANAGER = 'QMGR';
     process.env.MQ_QUEUE = 'my-fake-queue';
     process.env.MQ_USER = 'a-user';
     process.env.MQ_PASSWORD = 'a-password';
 
-    const URL = `https://${process.env.MQ_USER}:${process.env.MQ_PASSWORD}@${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v2/messaging/qmgr/${process.env.MQ_QUEUE_MANAGER}/queue/${process.env.MQ_QUEUE}/message`;
-  
-    it('should call the MQ API when a message event is triggered', async () => {
-    
     const event : SQSEvent = {
       Records: [
-          {
-              "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
-              "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
-              "body": "Test message.",
-              "attributes": {
-                  "ApproximateReceiveCount": "1",
-                  "SentTimestamp": "1545082649183",
-                  "SenderId": "AIDAIENQZJOLO23YVJ4VO",
-                  "ApproximateFirstReceiveTimestamp": "1545082649185"
-              },
-              "messageAttributes": {},
-              "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
-              "eventSource": "aws:sqs",
-              "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
-              "awsRegion": "us-east-2"
+        {
+          "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
+          "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
+          "body": "Test message.",
+          "attributes": {
+            "ApproximateReceiveCount": "1",
+            "SentTimestamp": "1545082649183",
+            "SenderId": "AIDAIENQZJOLO23YVJ4VO",
+            "ApproximateFirstReceiveTimestamp": "1545082649185"
           },
+          "messageAttributes": {},
+          "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
+          "eventSource": "aws:sqs",
+          "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
+          "awsRegion": "us-east-2"
+        },
+        {
+          "messageId": "2e1424d4-f796-459a-8184-9c92662be6da",
+          "receiptHandle": "AQEBzWwaftRI0KuVm4tP+/7q1rGgNqicHq...",
+          "body": "Test message.",
+          "attributes": {
+            "ApproximateReceiveCount": "1",
+            "SentTimestamp": "1545082650636",
+            "SenderId": "AIDAIENQZJOLO23YVJ4VO",
+            "ApproximateFirstReceiveTimestamp": "1545082650649"
+          },
+          "messageAttributes": {},
+          "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
+          "eventSource": "aws:sqs",
+          "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
+          "awsRegion": "us-east-2"
+        }
       ]
     };
-
     moxios.stubRequest(
       URL,
       {
@@ -55,70 +113,12 @@ describe('Test for sqs-payload-logger', function () {
     );
     await pushMessageToMq(event, axiosInstance);
 
-    const request = moxios.requests.mostRecent();
-    expect(request.config.method).toEqual("post");
-    expect(request.url)
+    event.Records.forEach((record, index) => {
+      const request = moxios.requests.at(index);
+      expect(request).toBeDefined();
+      expect(request.config.method).toEqual("post");
+      expect(request.url)
         .toEqual(URL);
-    expect(JSON.parse(request.config.data)).toEqual(event.Records[0]);
-  });
-
-  it('should call the MQ endpoint for each record supplied by the event', async () => {
-      process.env.MQ_HOST = 'a-host';
-      process.env.MQ_PORT = '1999';
-      process.env.MQ_QUEUE = 'my-fake-queue';
-      process.env.MQ_USER = 'a-user';
-      process.env.MQ_PASSWORD = 'a-password';
-
-      const event : SQSEvent = {
-          Records: [
-              {
-                  "messageId": "059f36b4-87a3-44ab-83d2-661975830a7d",
-                  "receiptHandle": "AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...",
-                  "body": "Test message.",
-                  "attributes": {
-                      "ApproximateReceiveCount": "1",
-                      "SentTimestamp": "1545082649183",
-                      "SenderId": "AIDAIENQZJOLO23YVJ4VO",
-                      "ApproximateFirstReceiveTimestamp": "1545082649185"
-                  },
-                  "messageAttributes": {},
-                  "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
-                  "eventSource": "aws:sqs",
-                  "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
-                  "awsRegion": "us-east-2"
-              },
-              {
-                  "messageId": "2e1424d4-f796-459a-8184-9c92662be6da",
-                  "receiptHandle": "AQEBzWwaftRI0KuVm4tP+/7q1rGgNqicHq...",
-                  "body": "Test message.",
-                  "attributes": {
-                      "ApproximateReceiveCount": "1",
-                      "SentTimestamp": "1545082650636",
-                      "SenderId": "AIDAIENQZJOLO23YVJ4VO",
-                      "ApproximateFirstReceiveTimestamp": "1545082650649"
-                  },
-                  "messageAttributes": {},
-                  "md5OfBody": "e4e68fb7bd0e697a0ae8f1bb342846b3",
-                  "eventSource": "aws:sqs",
-                  "eventSourceARN": "arn:aws:sqs:us-east-2:123456789012:my-queue",
-                  "awsRegion": "us-east-2"
-              }
-          ]
-      };
-      moxios.stubRequest(
-          URL,
-          {
-              status: 200,
-          }
-      );
-      await pushMessageToMq(event, axiosInstance);
-
-      event.Records.forEach((record, index) => {
-          const request = moxios.requests.at(index);
-          expect(request).toBeDefined();
-          expect(request.config.method).toEqual("post");
-          expect(request.url)
-              .toEqual(URL);
-      });
+    });
   });
 });
--- src/handlers/sqs-payload-logger.ts
+++ src/handlers/sqs-payload-logger.ts
@@ -3,26 +3,26 @@ import * as https from 'https';
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance = axios.create({
-    httpsAgent: new https.Agent({  
-      rejectUnauthorized: false
+  httpsAgent: new https.Agent({  
+    rejectUnauthorized: false
   })
 });
 
 export const pushMessageToMq = async (event: SQSEvent, instance: AxiosInstance = axiosInstance ): Promise<void> => {
   console.info(`Received ${event.Records.length} records`);
   await Promise.all(event.Records.map(async (record) => {
-    console.info("Posting record to MQ");
-    const URL = `https://${process.env.MQ_USER}:${process.env.MQ_PASSWORD}@${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v2/messaging/qmgr/${process.env.MQ_QUEUE_MANAGER}/queue/${process.env.MQ_QUEUE}/message`;
-    console.log(`Posting to: ${URL}`);
-    await instance.post(
-      URL,
-      record,
-      {
-        headers: {
-          "ibm-mq-rest-csrf-token": "blank",
-          "Content-Type": "text/plain;charset=utf-8"
-        },
-      }
-    )
+  console.info("Posting record to MQ");
+  const URL = `https://${process.env.MQ_USER}:${process.env.MQ_PASSWORD}@${process.env.MQ_HOST}:${process.env.MQ_PORT}/ibmmq/rest/v2/messaging/qmgr/${process.env.MQ_QUEUE_MANAGER}/queue/${process.env.MQ_QUEUE}/message`;
+  console.log(`Posting to: ${URL}`);
+  await instance.post(
+    URL,
+    record,
+    {
+    headers: {
+      "ibm-mq-rest-csrf-token": "blank",
+      "Content-Type": "text/plain;charset=utf-8"
+    },
+    }
+  )
   }));
 }
